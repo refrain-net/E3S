a43d554a5095727446db35039ca6503ecb22715e
refrain-net <72150044+refrain-net@users.noreply.github.com>
2020-10-17 11:58:59 +0900

    Update e3s-core@1.0.js

--- e3s-core@1.0.js
+++ e3s-core@1.0.js
@@ -2,7 +2,6 @@
  * @overview e3s-core@1.0.js E3Sの制御プログラム
  * @author Refrain <refrain.net@gmail.com>
  * @since 2020/10/1
- * @update 2020/10/15 pad0の廃止
  * @version 1.0
  * @copyright (c) Copyright 2020 Refrain All Rights Reserved.
  */
@@ -200,15 +199,14 @@ function format () {
   ];
 }
 /**
- * @deprecated ES8でのString#padStartが実装された為
  * @function pad0 数値の先頭を0で埋め、指定の桁数で返す
  * @argument {Number} value 0で埋める値
  * @argument {Number} length 切り取る桁数
  * @return {String} 0で埋めて切り取られた文字列
  */
-//function pad0 (value, length) {
-//  return ('0'.repeat(length) + value).slice(-length);
-//}
+function pad0 (value, length) {
+  return ('0'.repeat(length) + value).slice(-length);
+}
 /**
  * @function arr2table 得られた日程データをテーブルに出力する
  */
