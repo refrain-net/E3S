0929cd5cacdc1c8808738509503c82721c55d855
Refrain <72150044+refrain-net@users.noreply.github.com>
2021-07-28 01:10:40 +0900

    Update e3s-core@1.2.2.js

--- e3s-core@1.2.2.js
+++ e3s-core@1.2.2.js
@@ -49,26 +49,26 @@ let loopCount;
 let totalTime;
 
 /**
- * @function loadConfig コンフィグファイルの読み込み
- * @param {File} file 読み込み対象のファイル
+ * コンフィグファイルの読み込み
+ * @function loadConfig
+ * @param {File} file
  */
 async function loadConfig (file) {
   IM_LIST.length = 0;
   HO_LIST.length = 0;
   const text = await readFile(file);
   text.replace(/\r/g, '').split('\n').forEach(function (currentValue) {
-    if (IM_PATTERN.test(currentValue)) {
-      IM_LIST.push(currentValue.split(/[~_]/));
-    } else if (HO_PATTERN.test(currentValue)) {
-      HO_LIST.push(currentValue);
-    }
+    /** @summary 正規表現で値をテストし、結果に応じてデータを加工・格納する */
+    if (IM_PATTERN.test(currentValue)) IM_LIST.push(currentValue.split(/[~_]/));
+    else if (HO_PATTERN.test(currentValue)) HO_LIST.push(currentValue);
   });
 }
 
 /**
- * @function readFile ファイルを読み込む
- * @param {File} file 読み込む対象のFileオブジェクト
- * @returns {Promise} 読み込みを実行するPromiseオブジェクト
+ * ファイルを読み込む
+ * @function readFile
+ * @param {File} file
+ * @returns {Promise}
  */
 function readFile (file) {
   return new Promise(function (resolve, reject) {
@@ -84,7 +84,8 @@ function readFile (file) {
 }
 
 /**
- * @function init 全データの初期化処理
+ * 初期化処理
+ * @function init
  */
 function init () {
   baseDate = new Date(`${iophZzyF.value}/${GFZYmEFU.value}/${QR0Oq3bL.value} ${az1m1nnB.value}:${NMQr9RMs.value}`);
@@ -102,135 +103,53 @@ function init () {
 }
 
 /**
- * @function main 日程演算のメイン部
- * @description 1. 2周目以降でのみ補正を有効にする
- *              2. refDateを翌日に変更する
- *              3. 補正値を+24する
- *              4. 補正後の日付が休日の場合、翌日に変更する
- *              5. refDateのデータを保存する
- *              6. 0分に修正する
- *              7. 10時に設定する
- *              8. 経過時間を更新し、経過時間 < ループ回数 x スパンか、休日を検出する
- *              9. refDateを翌日に変更する
- *              10. 設備を停止させる日なら、trueを返して関数を抜け、親ループを継続する
- *              11. refDateのデータを保存する
- *              12. 経過時間と停止理由(停止させないので'')を保存する
- *              13. データをテーブルに出力する
- *              14. ループ回数を+1する
- *              15. 17時に設定する
- *              16. 経過時間が上限に達しているかを返し、親ループを制御する
+ * 日程演算のメイン部
+ * @function main
  */
 function main () {
-  if (loopCount > 0) {                                                                                    // 1
-    do {
-      refDate.setDate(refDate.getDate() + 1);                                                             // 2
-      spanOffset += 24;                                                                                   // 3
-    } while (checkHoliday());                                                                             // 4
+  if (loopCount > 0) addDateIfDateIsHoliday();
+  saveScheduleDateToCache();
+  refDate.setMinutes(0);
+  setDate(HOUR_OF_PAUSE);
+  /** @summary 経過時間を更新し、経過時間がループ回数 x スパン未満または休日であれば繰り返す */
+  while ((totalTime = getTotalTime() - spanOffset) < spanTime * (getLoopCount() + 1) || refDateIsHoliday()) {
+    refDate.setDate(refDate.getDate() + 1);
+    /** @summary 設備を停止させる日ならtrueを返す */
+    if (refDateIsImmobile()) return true;
   }
-  saveScheduleDateToCache();                                                                              // 5
-  refDate.setMinutes(0);                                                                                  // 6
-  setDate(HOUR_OF_PAUSE);                                                                                            // 7
-  while ((totalTime = getTotalTime() - spanOffset) < spanTime * (getLoopCount() + 1) || checkHoliday()) { // 8
-    refDate.setDate(refDate.getDate() + 1);                                                               // 9
-    if (checkImmobile()) {
-      return true;                                                                                        // 10
-    }
-  }
-  saveScheduleDateToCache();                                                                              // 11
-  CACHE_DATA.push(totalTime, totalTime - totalTime % spanTime);                                           // 12
-  printScheduleToTable();                                                                                 // 13
-  loopCount ++;                                                                                           // 14
-  setDate(HOUR_OF_RESTART);                                                                                            // 15
-  return totalTime < limitTime;                                                                           // 16
+  saveScheduleDateToCache();
+  CACHE_DATA.push(totalTime, totalTime - totalTime % spanTime);
+  printScheduleToTable();
+  loopCount ++;
+  setDate(HOUR_OF_RESTART);
+  /** @summary 経過時間が上限に達しているかを返す */
+  return totalTime < limitTime;
 }
 
 /**
- * @function checkHoliday 参照中の日時が休日かを判定する
- * @returns {boolean} 参照中の日時が休日か否か
+ * refDateが休日であれば、refDateを翌日に変更する
+ * @function addDateIfDateIsHoliday
  */
-function checkHoliday () {
+function addDateIfDateIsHoliday () {
+  do {
+    refDate.setDate(refDate.getDate() + 1);
+    spanOffset += 24;
+  } while (refDateIsHoliday());
+}
+
+/**
+ * refDateが休日かを判定する
+ * @function refDateIsHoliday
+ * @returns {boolean}
+ */
+function refDateIsHoliday () {
   return HO_LIST.includes(getFormatedDateValue()[0]) || refDate.isWeekend();
 }
 
 /**
- * @function saveScheduleDateToCache refDateの各値を保存する
- */
-function saveScheduleDateToCache () {
-  CACHE_DATA.push(refDate.getFullYear(), refDate.getMonth() + 1, refDate.getDate(), refDate.getHours(), refDate.getMinutes());
-}
-
-/**
- * @function setDate refDateを任意の時間に設定する
- * @param {number} hour 設定する時間
- */
-function setDate (hour) {
-  hour %= 24;
-  if (refDate.getHours() > hour) {
-    refDate.setDate(refDate.getDate() + 1);
-  }
-  refDate.setHours(hour);
-}
-
-/**
- * @function getTotalTime 経過時間を取得する
- * @returns {number} (現在時刻 - 開始時刻) - 中断時間 x ループ回数 - 停止時間 + 初期時間
- */
-function getTotalTime () {
-  return ms2hr(refDate.getTime() - baseDate.getTime()) - (HOUR_OF_RESTART - HOUR_OF_PAUSE) * getLoopCount() - disableTime + baseTime;
-}
-
-/**
- * @function ms2hr ミリ秒を時間に変換する
- * @param {number} milliseconds 変換するミリ秒
- * @returns {number} 変換後の時間
- */
-function ms2hr (milliseconds) {
-  return (milliseconds / 3600000) | 0;
-}
-
-/**
- * @function getLoopCount 繰り返し回数を取得する
- * @returns {number} 加算される繰り返し回数 + 繰り返し回数の補正値
- */
-function getLoopCount () {
-  return loopCount + loopOffset;
-}
-
-/**
- * @function checkImmobile imListに含まれる場合に計算を中断させる
- * @returns {boolean} 参照日時がimListに含まれていたか
- * @description 1. imListをループする
- *              2. refDateの日付と停止日が一致しなければループを再開する
- *              3. 停止日でDateオブジェクトを作成する
- *              4. stopDateのデータを保存する
- *              5. 経過時間を更新する
- *              6. 経過時間と停止理由を保存する
- *              7. データをテーブルに出力する
- *              8. 再開日でDateオブジェクトを作成する
- *              9. 再開日 - 停止日で停止させていた時間を取得する
- *              10. refDateを更新する
- *              11. trueを返し関数を終了し、親ループを終了する
- *              12. falseを返し関数を終了し、親ループを継続する
- */
-function checkImmobile () {
-  for (const [stop, restart, reason] of IM_LIST) {                  // 1
-    if (getFormatedDateValue()[0] !== stop.split(' ')[0]) continue; // 2
-    refDate = new Date(stop);                                       // 3
-    saveScheduleDateToCache();                                      // 4
-    totalTime = getTotalTime();                                     // 5
-    CACHE_DATA.push(totalTime, reason);                             // 6
-    printScheduleToTable();                                         // 7
-    const temp = new Date(restart);                                 // 8
-    disableTime += ms2hr(temp.getTime() - refDate.getTime());       // 9
-    refDate = temp;                                                 // 10
-    return true;                                                    // 11
-  }
-  return false;                                                     // 12
-}
-
-/**
- * @function getFormatedDateValue refDateを[YYYY/MM/DD, hh:mm]形式に変換する
- * @returns {string[]} [YYYY/MM/DD, hh:mm]形式に変換されたrefDate
+ * refDateを[YYYY/MM/DD, hh:mm]形式に変換する
+ * @function getFormatedDateValue
+ * @returns {string[]}
  */
 function getFormatedDateValue () {
   const ymd = `${refDate.getFullYear()}/${`${refDate.getMonth() + 1}`.padStart(2, '0')}/${`${refDate.getDate()}`.padStart(2, '0')}`;
@@ -239,7 +158,77 @@ function getFormatedDateValue () {
 }
 
 /**
- * @function printScheduleToTable 得られた日程データをテーブルに出力する
+ * refDateの値を保存する
+ * @function saveScheduleDateToCache
+ */
+function saveScheduleDateToCache () {
+  CACHE_DATA.push(refDate.getFullYear(), refDate.getMonth() + 1, refDate.getDate(), refDate.getHours(), refDate.getMinutes());
+}
+
+/**
+ * refDateを任意の時間に設定する
+ * @function setDate
+ * @param {number} hour
+ */
+function setDate (hour) {
+  hour %= 24;
+  if (refDate.getHours() > hour) refDate.setDate(refDate.getDate() + 1);
+  refDate.setHours(hour);
+}
+
+/**
+ * 経過時間を取得する
+ * @function getTotalTime
+ * @returns {number} (現在時刻 - 開始時刻) - 中断させていた時間 x ループ回数 - 停止させていた時間 + 初期に経過していた時間
+ */
+function getTotalTime () {
+  return ms2hr(refDate.getTime() - baseDate.getTime()) - (HOUR_OF_RESTART - HOUR_OF_PAUSE) * getLoopCount() - disableTime + baseTime;
+}
+
+/**
+ * ミリ秒を時間に変換する
+ * @function ms2hr
+ * @param {number} milliseconds
+ * @returns {number}
+ */
+function ms2hr (milliseconds) {
+  return (milliseconds / 3600000) | 0;
+}
+
+/**
+ * 繰り返し回数を取得する
+ * @function getLoopCount
+ * @returns {number} 繰り返し回数 + 補正値
+ */
+function getLoopCount () {
+  return loopCount + loopOffset;
+}
+
+/**
+ * refDateがimListに含まれるかを判定する
+ * @function refDateIsImmobile
+ * @returns {boolean}
+ */
+function refDateIsImmobile () {
+  for (const [stop, restart, reason] of IM_LIST) {
+    /** @summary refDateの日付と停止日が一致しなければループを再開する */
+    if (getFormatedDateValue()[0] !== stop.split(' ')[0]) continue;
+    refDate = new Date(stop);
+    saveScheduleDateToCache();
+    totalTime = getTotalTime();
+    CACHE_DATA.push(totalTime, reason);
+    printScheduleToTable();
+    const temp = new Date(restart);
+    disableTime += ms2hr(temp.getTime() - refDate.getTime());
+    refDate = temp;
+    return true;
+  }
+  return false;
+}
+
+/**
+ * 得られた日程データをテーブルに出力する
+ * @function printScheduleToTable
  */
 function printScheduleToTable () {
   const tr = document.createElement('tr');
